52f45bd9b469ede83353873770b15d481fd32703
ArthurEly <arthur.ely78@gmail.com>
2023-08-01 23:20:49 -0300

    adicionando um exemplo de tela

---
+++ IniciarJogo.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class IniciarJogo extends JFrame {
+
+    public static void main(String[] args) {
+        new IniciarJogo();
+    }
+
+    IniciarJogo() {
+        add(new TelaJogo());
+        setTitle("Jogo da Cobrinha - Snake game");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+}
